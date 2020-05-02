70412154e06599f21b00c3376c2a074e5ea8bbdd
vtrrix <viveksonybcd@gmail.com>
2020-05-02 17:58:08 +0530

    some styling in about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function About() {
-    return (
-        <React.Fragment>
-            <h1>
-                About
-            </h1>
-            <p>
-                Hello there ! My name is vtrix and this is a todo list application made by me using reactjs.
-            </p>
-        </React.Fragment>
-    )
+	return (
+		<React.Fragment>
+			<h1 style={{ margin: "10px", textAlign: "center" }}>About</h1>
+			<p style={{ margin: "10px" }}>
+				Hello there ! My name is vtrix and this is a todo list application made
+				by me using reactjs.
+			</p>
+		</React.Fragment>
+	);
 }
-
 
 export default About;
